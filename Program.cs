a638b1260883cfe1ca432f1b9a38ec03c9fe2251
GB2101 <gabrielbrazcs@gmail.com>
2021-01-08 17:52:53 -0300

    Creates Vector class

--- Program.cs
+++ Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using School.classes;
 
 namespace School
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Vector<string> vec = new Vector<string>();
+			vec.push("3");
+			vec.push("8");
+			vec.push("5");
+			vec.push("7");
+			vec.push("1");
+			vec.push("9");
+			vec.push("4");
+			vec.push("2");
+			vec.push("0");
+			vec.push("6");
+			vec.push("Nome1");
+			vec.push("Nome2");
+			vec.push("Nome3");
+
+			Console.WriteLine(vec[6]);
+			Console.WriteLine(vec[4]);
+			Console.WriteLine(vec[0]);
+			Console.WriteLine(vec.pop());
+			Console.WriteLine(vec.pop());
+			Console.WriteLine(vec.pop());
+
+			vec.push("teste");
+			Console.WriteLine(vec.pop());
+			Console.WriteLine(vec.length);
+
+			Vector<string> vec2 = new Vector<string>();
+			vec2.push("Teste1");
+			vec2.push("Teste2");
+
+			vec.concat(vec2);
+			Console.WriteLine(vec.pop());
+			Console.WriteLine(vec.pop());
+			Console.WriteLine(vec.pop());
+			Console.WriteLine(vec.pop());
+
+		}
+	}
 }
---
+++ classes/Vector.cs
@@ -0,0 +1,102 @@
+namespace School.classes
+{
+	public class Vector<T>
+	{
+		private T[] _array;
+		private int _size;
+		private int _length;
+
+		public int length { get { return this._length; } }
+
+		public Vector()
+		{
+			this._size = 25;
+			this._length = 0;
+			this._array = new T[this._size];
+
+		}
+
+		public T this[int i]
+		{
+			get
+			{
+				if (i < this._length)
+				{
+					return this._array[i];
+				}
+				else
+				{
+					throw new System.Exception("Index out of bounds");
+				}
+			}
+			set
+			{
+				if (i < this._length)
+				{
+					this._array[i] = value;
+				}
+				else
+				{
+					throw new System.Exception("Index out of bounds");
+				}
+			}
+		}
+
+		public void push(T item)
+		{
+			this._array[this._length] = item;
+			this._length++;
+			this._validateSize();
+		}
+
+		public T pop()
+		{
+			var item = this._array[this._length - 1];
+			this._length--;
+			return item;
+		}
+
+		public T item(int index)
+		{
+			return this._array[index];
+		}
+
+		public void concat(T[] array)
+		{
+			int length = array.Length;
+			int pos = this._length;
+			this._length += length;
+			this._validateSize();
+			for (int i = 0; i < length; i++, pos++)
+			{
+				this._array[pos] = array[i];
+			}
+		}
+
+		public void concat(Vector<T> array)
+		{
+			int length = array.length;
+			int pos = this._length;
+			this._length += length;
+			this._validateSize();
+			for (int i = 0; i < length; i++, pos++)
+			{
+				this._array[pos] = array[i];
+			}
+		}
+
+		private void _validateSize()
+		{
+			if (this._length >= this._size)
+			{
+				this._size *= 2;
+				T[] temp = new T[this._size];
+				for (int i = 0; i < this._length; i++)
+				{
+					temp[i] = this._array[i];
+				}
+				this._array = temp;
+			}
+		}
+	}
+}
